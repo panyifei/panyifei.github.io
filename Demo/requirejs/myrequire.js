00a640b5ddb3b538072f42232f0deebbd68ca346
panyifei <981909093@qq.com>
2016-04-18 19:08:19 +0800

    改了一版requirejs

--- Demo/requirejs/myrequire.js
+++ Demo/requirejs/myrequire.js
@@ -1,31 +1,71 @@
 //allModule用来保存所有加载的模块
 var allModule = {};
 //主要的define方法
-var define = function(id,array,cb){
-    var length = array.length;
-    if(length > 0){
-        var i = 0;
-        array.forEach(function(value,index,array){
-            var tempScript = document.createElement('script');
-            tempScript.src = array[index]+'.js';
-            //目前用的onload来监听加载完毕，但是如果有依赖的话，就不行了
-            tempScript.onload = function(){
-                i++;
-                finish();
-            }
-            document.body.appendChild(tempScript);
-        })
-        function finish(){
-            if(i == length){
-                var modules =[];
-                for(var x = 0 ; x<array.length;x++){
-                    modules[x] = allModule[array[x]];
-                }
-                allModule[id] = cb.apply(null ,modules);
+function _registerModule(id,dependence,father){
+    if(!allModule[id]){
+        allModule[id]={};
+        allModule[id].id = id;
+        allModule[id].func = undefined;
+        allModule[id].dependence = dependence;
+        allModule[id].dependenceLoadNum = 0;
+        allModule[id].finishLoad = function(){};
+        if(father){
+            if(allModule[id].referrer){
+                allModule[id].referrer.push(father);
+            }else{
+                allModule[id].referrer = [];
+                allModule[id].referrer.push(father);
             }
         }
     }else{
-        allModule[id] = cb();
+        if(father){
+            allModule[id].referrer.push(father);
+        }
+        if(dependence){
+            allModule[id].dependence = dependence;
+            allModule[id].dependenceLoadNum = 0;
+        }
+    }
+}
+
+//cb为加载完了执行的方法
+//func为执行完了的方法
+var define = function(id,array,cb){
+    _registerModule(id,array,'',cb);
+    array.forEach(function(value,index,array){
+        _registerModule(array[index],[],id,cb);
+    });
+
+    var length = array.length;
+    if(length > 0){
+        array.forEach(function(value,index,array){
+            var tempScript = document.createElement('script');
+            tempScript.src = array[index]+'.js';
+            document.body.appendChild(tempScript);
+        })
+        allModule[id].finishLoad = finish;
+        function finish(){
+            allModule[id].dependenceLoadNum++;
+            if(allModule[id].dependenceLoadNum == allModule[id].dependence.length){
+                var modules =[];
+                for(var x = 0 ; x<array.length;x++){
+                    modules[x] = allModule[array[x]].func;
+                }
+                allModule[id].func = cb.apply(null ,modules);
+            }
+            if(allModule[id].referrer){
+                allModule[id].referrer.forEach(function(value,index,array){
+                    allModule[array[index]].finishLoad();
+                });
+            }
+        }
+    }else{
+        allModule[id].func = cb();
+        if(allModule[id].referrer){
+            allModule[id].referrer.forEach(function(value,index,array){
+                allModule[array[index]].finishLoad();
+            });
+        }
     }
 };
 
--- 框架以及规范/模块引入/Requirejs介绍以及源码阅读.md
+++ 框架以及规范/模块引入/Requirejs介绍以及源码阅读.md
@@ -99,6 +99,54 @@ require(['domready!'], function (doc){
 ### 写一个require
 在阅读源码之前，先试着实现一把。[链接](https://github.com/panyifei/Front-end-learning/tree/master/Demo)
 
+```javascript
+//allModule用来保存所有加载的模块
+var allModule = {};
+//主要的define方法
+var define = function(id,array,cb){
+    var length = array.length;
+    if(length > 0){
+        var i = 0;
+        array.forEach(function(value,index,array){
+            var tempScript = document.createElement('script');
+            tempScript.src = array[index]+'.js';
+            //目前用的onload来监听加载完毕，但是如果有依赖的话，就不行了
+            tempScript.onload = function(){
+                i++;
+                finish();
+            }
+            document.body.appendChild(tempScript);
+        })
+        function finish(){
+            if(i == length){
+                var modules =[];
+                for(var x = 0 ; x<array.length;x++){
+                    modules[x] = allModule[array[x]];
+                }
+                allModule[id] = cb.apply(null ,modules);
+            }
+        }
+    }else{
+        allModule[id] = cb();
+    }
+};
+var scripts = document.getElementsByTagName('script');
+var sLength = scripts.length;
+var mainJs;
+for(var i=0;i<sLength;i++){
+    var name = scripts[i].getAttribute("data-main");
+    if(name){
+        mainJs = name;
+    }
+}
+var mainScript = document.createElement('script');
+mainScript.src = mainJs;
+document.body.appendChild(mainScript);
+```
+
+重新修改了一次
+
+
 
 参考：
 
